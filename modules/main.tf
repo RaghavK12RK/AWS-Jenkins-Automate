2eecf4c841cf08fc4735d84630dd72aae4ec99a9
RaghavK12RK <109405973+RaghavK12RK@users.noreply.github.com>
2022-08-29 11:28:32 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+region = var.region
+access_key = var.access_key
+secret_key = var.secret_key
+}
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+resource "aws_vpc" "vpc_name" {
+cidr_block = "10.0.0.0/16" # cidr block iteration found in the terraform.tfvars file
+tags = {
+Name = "vpc-test"
+}
+}
 
