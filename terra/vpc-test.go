1bb337af38d22b61eaac4a16db8842b2b6762471
RaghavK12RK <109405973+RaghavK12RK@users.noreply.github.com>
2022-08-29 15:25:48 +0530

    Update vpc-test.go

--- terra/vpc-test.go
+++ terra/vpc-test.go
@@ -5,29 +5,32 @@ package test
 //we are using serveral libraries here for testing
 import (
 	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 //writing function to perform the test
 
-func TestTerraformVpcExample(t *testing.T){
+func TestVpcNameCreate(t *testing.T) {
+	t.Parallel()
 
-	terraformOptions := terraform.WithDefault.RetryableErrors(t, &terraform.Options{
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 
-	Terraform.Dir: "../modules	",
-	
-	Vars: map[string]interface{}{
-		"region"   = "us-east-1",
-		"vpc_name" = "test-vpc",
-	},
-})
-   //Run a Terraform init and apply from terraform options
-   terraform.InitAndApply(t, terraformOptions)
-   
-   //Run a Terraform destroy at the end of the test
-   defer terraform.Destroy(t, terraformOptions)
+		TerraformDir: "..//modules",
 
-   vpcArn := terraform.Output(t, terraform options, "")
-   vpcId := terraform.Output(t, terraform options, "")
+		Vars: map[string]interface{}{
+			"region":   "us-east-1",
+			"vpc_name": "test-vpc",
+		},
+	})
+	//Run a Terraform init and apply from terraform options
+	terraform.InitAndApply(t, terraformOptions)
 
+	//Run a Terraform destroy at the end of the test
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	output := terraform.Output(t, terraformOptions, "aws_vpc")
+	assert.Equal(t, "aws_vpc", output)
 }
