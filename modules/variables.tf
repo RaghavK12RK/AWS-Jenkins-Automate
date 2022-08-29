64d071bb44e544847fd5d10987a13c443577b67c
RaghavK12RK <109405973+RaghavK12RK@users.noreply.github.com>
2022-08-29 11:28:57 +0530

    Create variables.tf

---
+++ modules/variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+    default = "AKIAXVCQQMJA2ZXAOGPM"
+}
+
+variable "secret_key" {
+    default = "5b9XJav6MohYq0lmy5B/HEaYjkeHZ704UWw+dybz"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "test vpc name "
+  default     = "test-vpc"
+}
