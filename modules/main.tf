e164f0a8860089baabab03d2e9e6f6bc2d8ab794
RaghavK12RK <109405973+RaghavK12RK@users.noreply.github.com>
2022-08-30 12:25:53 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,17 +1,50 @@
-provider "aws" {
-region = var.region
-access_key = var.access_key
-secret_key = var.secret_key
-}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CREATE AN DD ORGANIZATION WITH USER AND ROLE ATTACHMENT MODULE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = ">= 0.12"
+  # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
+  # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
+  required_version = ">= 0.13"
+
+  required_providers {
+   datadog = {
+      source = "DataDog/datadog"
+      version = ">=3.0.0"
+    }
+  }
 }
 
-resource "aws_vpc" "vpc_name" {
-cidr_block = "10.0.0.0/16" # cidr block iteration found in the terraform.tfvars file
-tags = {
-Name = "vpc-test"
+# Configure the Datadog provider
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
 }
+
+# ----------------------------------------------------------------------------------------------------------------------
+# RESOURCE SECTION
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Create a new Datadog Child Organization
+resource "datadog_child_organization" "New_organization_name" {
+  name = "${var.BU-L2_shortname}-${var.BU-L3_shortname}"
 }
+
+# Creating an Datadog user with the role attachment
+data "datadog_role" "permission" {
+  filter = var.dd_role
+}
+
+# Create a new Datadog user
+resource "datadog_user" "user_email" {
+  email = var.tmna_email_id
+  roles = [data.datadog_role.permission.id]
+}
+
+
 
