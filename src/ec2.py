520c3a33e6637edd99f68c6d7c90a37fc1b80238
jenkins-master <109405973+RaghavK12RK@users.noreply.github.com>
2024-07-14 13:33:01 +0530

    Update ec2.py

--- src/ec2.py
+++ src/ec2.py
@@ -2,36 +2,42 @@ import boto3
 import logging
 import os
 
-ec2_names = ["test-ins", "dev-instance"]
+logging.basicConfig(
+      level=logging.INFO,
+      format='%{asctime}s - %{levelname}s - %{message}s',
+      style='{'
+)
+
+# ec2_names = ["test-ins"]
 
 def create_ec2():
 
-    ec2_client = boto3.client(
+    ec2_client = boto3.resource(
         'ec2',
         aws_access_key_id = "",
         aws_secret_access_key = "",
-        aws_session_token = "",
+        # aws_session_token = "",
         region_name = "us-west-2"
         )
     
-    ec2_response = ec2_client.run_instances(
+    try:
+     ec2_response = ec2_client.create_instances(
         MinCount = 1,
-        MaxCount = 2,
-        ImageId = 'ami-6cd6f714',
-        InstanceType = 't2.micro',
-        KeyName = 'test-key',
+        MaxCount = 1,
+        ImageId = 'ami-078701cc0905d44e4',
+        InstanceType = 't2.nano',
+        # KeyName = 'test-key',
         Monitoring = {
             'Enabled': True
         },
-        SubnetId = 'subnet-0f56ea93e0b3bd84f',
+        # SubnetId = 'subnet-0f56ea93e0b3bd84f',
         Placement = {
             'AvailabilityZone': 'us-west-2a'
         }
     )
-
-    for instance in ec2_response:
-        print(f"New instance created: {instance}")
-
-    print(ec2_response)     
+     logging.info(f"EC2 created {ec2_response}")
+     
+    except:  
+        logging.info("No instances created")    
 
 create_ec2()        
